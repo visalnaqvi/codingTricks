23daed3c1d3eb19cbf6b8fb0f5c0c70cfca14cc2
unknown <visalnaqvi@gmail.com>
2022-08-28 20:27:30 +0530

    first commit

--- comps/item-preview/grid.js
+++ comps/item-preview/grid.js
@@ -1,11 +1,7 @@
 import Link from "next/link";
 import style from "../../styles/itemPreview.module.css"
 import text from '../../styles/Text.module.css'
-import Image from "next/image"
-import video1 from "../../public/video1.gif"
-import video2 from "../../public/video2.gif"
-import video3 from "../../public/video3.gif"
-import { useEffect } from "react";
+
 const ItemPreviewGrid = () => {
     // useEffect(() => {
     //    document.querySelector('.video').play();
@@ -14,7 +10,7 @@ const ItemPreviewGrid = () => {
         <div className={style.mainWrapper}>
             <Link href="/products/navigation-bars/side-navigation-floating">
                 <div className={style.card}>
-                    <div className={style.video}><Image width={320} height={180} src={video1} alt="balle"></Image></div>
+                    <div className={style.video}><video muted loop autoPlay src="/video1.webm"></video></div>
                     <div className={style.content}>
                         <h2 className={text.cardh2}>HTM, CSS, JavaScript Navigation Bar</h2>
                         <button className={style.button}>Get Code</button>
@@ -23,7 +19,7 @@ const ItemPreviewGrid = () => {
             </Link>
             <Link href="/products/action-buttons/action-button">
             <div className={style.card}>
-                <div className={style.video}><Image width={320} height={180} src={video2} alt="balle"></Image></div>
+                <div className={style.video}><video muted loop autoPlay src="/video2.webm"></video></div>
                 <div className={style.content}>
         
                     <h2 className={text.cardh2}>HTM, CSS, JavaScript Action Bar</h2>
@@ -33,7 +29,7 @@ const ItemPreviewGrid = () => {
             </Link>
             <Link href="/products/action-buttons/action-button-red-2">
             <div className={style.card}>
-                <div className={style.video}><Image width={320} height={180} src={video3} alt="balle"></Image></div>
+                <div className={style.video}><video muted loop autoPlay src="/video3.webm"></video></div>
                 <div className={style.content}>
                     <h2 className={text.cardh2}>HTM, CSS, JavaScript Action Button</h2>
                     <button className={style.button}>Get Code</button>
